c2a73327b20b5091500b66734e4e0c9db98d161d
Christoph <c.wagner.germany@gmail.com>
2025-07-26 18:18:35 +0200

    btn

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, computed, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule],
+  imports: [MatToolbarModule, MatIconModule, RouterOutlet, MatSidenavModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
