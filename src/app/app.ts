667af240e8253b87580f80ea6181a8dcccb26614
Christoph <c.wagner.germany@gmail.com>
2025-07-26 18:13:31 +0200

    side

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('crud');
+
+  collapsed = signal(false);
+
+  sidenavWidth = computed(() => (this.collapsed() ? '65px' : '250px'));
 }
