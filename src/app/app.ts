548ad289d55b27684815261b64104b5b511a9022
Christoph <c.wagner.germany@gmail.com>
2025-07-29 16:27:07 +0200

    darkmode toggle working

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,23 +5,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
-import { Theme, ThemeService } from './serverices/theme-service';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    MatToolbarModule,
-    MatIconModule,
-    RouterOutlet,
-    MatSidenavModule,
-    MatButtonModule,
-    MatSlideToggleModule,
-    AsyncPipe
-  ],
+  imports: [MatToolbarModule, MatIconModule, MatSidenavModule, MatButtonModule, MatSlideToggleModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('crud');
@@ -35,16 +24,6 @@ export class App {
   applyDarkMode = effect(() => {
     const darkMode = this.darkMode();
     document.body.classList.toggle('darkMode', darkMode);
-  });
-  currentTheme$!: Observable<Theme>;
+  })
 
-  constructor(private themeService: ThemeService) {}
-
-  ngOnInit(): void {
-    this.currentTheme$ = this.themeService.currentTheme$;
-  }
-
-  toggleTheme(): void {
-    this.themeService.toggleTheme();
-  }
 }
--- src/app/serverices/theme-service.ts
+++ src/app/serverices/theme-service.ts
@@ -1,59 +1,8 @@
-import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-
-export type Theme = 'light' | 'dark';
+import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ThemeService {
-  private renderer: Renderer2;
-  private _currentTheme: BehaviorSubject<Theme>;
-  private readonly THEME_KEY = 'appTheme';
-
-  constructor(private rendererFactory: RendererFactory2) {
-    this.renderer = this.rendererFactory.createRenderer(null, null);
-    // Initialisierung des Themes basierend auf localStorage oder Systempräferenz
-    const savedTheme = localStorage.getItem(this.THEME_KEY) as Theme;
-    const prefersDark =
-      window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const initialTheme: Theme = savedTheme || (prefersDark ? 'dark' : 'light');
-
-    this._currentTheme = new BehaviorSubject<Theme>(initialTheme);
-    this.applyTheme(initialTheme); // Initial das Theme anwenden
-  }
-
-  get currentTheme$() {
-    return this._currentTheme.asObservable();
-  }
-
-  get currentTheme(): Theme {
-    return this._currentTheme.value;
-  }
-
-  setTheme(theme: Theme): void {
-    if (this._currentTheme.value === theme) {
-      return; // Theme ist bereits gesetzt, keine Änderung notwendig
-    }
-    this.applyTheme(theme);
-    this._currentTheme.next(theme);
-    localStorage.setItem(this.THEME_KEY, theme);
-  }
-
-  toggleTheme(): void {
-    const newTheme = this.currentTheme === 'light' ? 'dark' : 'light';
-    this.setTheme(newTheme);
-  }
-
-  private applyTheme(theme: Theme): void {
-    const body = document.body;
-    if (theme === 'dark') {
-      this.renderer.addClass(body, 'my-dark-theme');
-      this.renderer.removeClass(body, 'my-light-theme');
-    } else {
-      this.renderer.addClass(body, 'my-light-theme');
-      this.renderer.removeClass(body, 'my-dark-theme');
-    }
-  }
+  
 }
