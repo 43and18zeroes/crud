2e082f49af3c7abd38b076dbc4f34e47282b7c5d
Christoph <c.wagner.germany@gmail.com>
2025-07-29 16:20:01 +0200

    palette

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,12 +5,23 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { Theme, ThemeService } from './serverices/theme-service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, MatIconModule, RouterOutlet, MatSidenavModule, MatButtonModule, MatSlideToggleModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    RouterOutlet,
+    MatSidenavModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    AsyncPipe
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('crud');
@@ -24,6 +35,16 @@ export class App {
   applyDarkMode = effect(() => {
     const darkMode = this.darkMode();
     document.body.classList.toggle('darkMode', darkMode);
-  })
+  });
+  currentTheme$!: Observable<Theme>;
 
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.currentTheme$ = this.themeService.currentTheme$;
+  }
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
 }
